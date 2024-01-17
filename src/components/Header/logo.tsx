90936522cca70f67f74700bc218cfec1a80227ba
Dominic Leon Hackett <dominichackett@gmail.com>
2024-01-16 20:23:40 -0400

    Initial Push
    Galactic Gambit Initial Push

---
+++ src/components/Header/logo.tsx
@@ -0,0 +1,42 @@
+export default function Logo(){
+    return(<svg xmlns="http://www.w3.org/2000/svg" width="120px" height="60px" viewBox="0 0 120 60">
+	<rect width="120" height="60" fill="transparent" />
+  
+	<rect width="50" height="50" fill="#E5E5E5" x="10" y="5" />
+	<circle cx="25" cy="15" r="5" fill="#000" />
+	<circle cx="45" cy="15" r="5" fill="#000" />
+	<circle cx="25" cy="30" r="5" fill="#000" />
+	<circle cx="45" cy="30" r="5" fill="#000" />
+	<circle cx="25" cy="45" r="5" fill="#000" />
+	<circle cx="45" cy="45" r="5" fill="#000" />
+  
+	<polygon points="10,5 60,5 35,-20" fill="#BDBDBD" />
+  
+	<rect width="50" height="50" fill="#E5E5E5" x="70" y="5" />
+	<circle cx="85" cy="15" r="5" fill="#000" />
+	<circle cx="105" cy="15" r="5" fill="#000" />
+	<circle cx="85" cy="30" r="5" fill="#000" />
+	<circle cx="105" cy="30" r="5" fill="#000" />
+	<circle cx="85" cy="45" r="5" fill="#000" />
+	<circle cx="105" cy="45" r="5" fill="#000" />
+  
+	<polygon points="70,5 120,5 95,-20" fill="#BDBDBD" />
+  </svg>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  )
+}
